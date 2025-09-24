a03bdbd09787a0be932d3a03fad4adea84fa937f
Anton Fridlund <anton.fridlund@hotmail.se>
2025-09-24 03:15:17 +0200

    Add validate function

--- src/Models/Users/UserDto.cs
+++ src/Models/Users/UserDto.cs
@@ -1,8 +1,17 @@
 namespace Models.Users;
 
-public record UserDto(
+public partial record UserDto(
     int Id,
     string FirstName,
     string LastName,
     string Username
 );
+
+public partial record UserDto {
+  public string? Validate() {
+    if (string.IsNullOrWhiteSpace(FirstName)) return "First name required";
+    if (string.IsNullOrWhiteSpace(LastName)) return "Last name required";
+    if (string.IsNullOrWhiteSpace(Username)) return "Username required";
+    return null;
+  }
+}
--- src/Models/Users/UserModel.cs
+++ src/Models/Users/UserModel.cs
@@ -1,9 +1,19 @@
 namespace Models.Users;
 
-public record UserModel(
+public partial record UserModel(
     int Id,
     string FirstName,
     string LastName,
     string Username,
     string Password
 );
+
+public partial record UserModel {
+  public string? Validate() {
+    if (string.IsNullOrWhiteSpace(FirstName)) return "First name required";
+    if (string.IsNullOrWhiteSpace(LastName)) return "Last name required";
+    if (string.IsNullOrWhiteSpace(Username)) return "Username required";
+    if (string.IsNullOrWhiteSpace(Password)) return "Password required";
+    return null;
+  }
+}
