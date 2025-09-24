78ddc551588ed7b282472b073051c557ab0b7230
Anton Fridlund <anton.fridlund@hotmail.se>
2025-09-24 02:24:38 +0200

    Remove nullable fields

--- src/Models/Users/UserDto.cs
+++ src/Models/Users/UserDto.cs
@@ -1,8 +1,8 @@
 namespace Models.Users;
 
 public record UserDto(
-    int? Id,
-    string? FirstName,
-    string? LastName,
-    string? Username
+    int Id,
+    string FirstName,
+    string LastName,
+    string Username
 );
--- src/Models/Users/UserModel.cs
+++ src/Models/Users/UserModel.cs
@@ -1,9 +1,9 @@
 namespace Models.Users;
 
 public record UserModel(
-    int? Id,
-    string? FirstName,
-    string? LastName,
-    string? Username,
-    string? Password
+    int Id,
+    string FirstName,
+    string LastName,
+    string Username,
+    string Password
 );
