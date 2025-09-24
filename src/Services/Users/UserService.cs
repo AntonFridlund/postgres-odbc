fd1d0a865cf85275037b8eaed41f58c8fd9e6843
Anton Fridlund <anton.fridlund@hotmail.se>
2025-09-24 02:27:49 +0200

    Simplify query and response

--- src/Services/Users/UserService.cs
+++ src/Services/Users/UserService.cs
@@ -4,20 +4,14 @@ using Configs;
 
 namespace Services.Users;
 
-public interface IUserService {
-  Task<UserDto?> GetUserByIdAsync(int id);
-}
-
 public class UserService : IUserService {
   public async Task<UserDto?> GetUserByIdAsync(int id) {
     using var pool = await Postgres.GetPoolAsync();
     await using var cmd = pool.Connection.CreateCommand();
-
     cmd.CommandText = @"
-            WITH params AS (SELECT ? id)
-            SELECT u.id, u.first_name, u.last_name, u.user_name
-            FROM user_data.users u, params p
-            WHERE u.id = p.id";
+      SELECT id, first_name, last_name, user_name
+      FROM user_data.users
+      WHERE id = ?";
     cmd.Parameters.Add(new() { OdbcType = OdbcType.Int, Value = id });
     await using var reader = await cmd.ExecuteReaderAsync();
 
@@ -28,11 +22,11 @@ public class UserService : IUserService {
 
     if (await reader.ReadAsync()) {
       return new UserDto(
-        reader.IsDBNull(usrIdOrdinal) ? null : reader.GetInt32(usrIdOrdinal),
-        reader.IsDBNull(fNameOrdinal) ? null : reader.GetString(fNameOrdinal),
-        reader.IsDBNull(lNameOrdinal) ? null : reader.GetString(lNameOrdinal),
-        reader.IsDBNull(unameOrdinal) ? null : reader.GetString(unameOrdinal)
-        );
+        reader.GetInt32(usrIdOrdinal),
+        reader.GetString(fNameOrdinal),
+        reader.GetString(lNameOrdinal),
+        reader.GetString(unameOrdinal)
+      );
     } else return null;
   }
 }
