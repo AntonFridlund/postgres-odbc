2fdc7603c83febc8b6aa7bd50d2b1944d22d16ff
Anton Fridlund <anton.fridlund@hotmail.se>
2025-10-02 22:34:04 +0200

    Add error handling for json decode

--- src/Controllers/Users/UserController.cs
+++ src/Controllers/Users/UserController.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using Models.Users;
 using Services.Users;
 
@@ -5,25 +6,26 @@ namespace Controllers.Users;
 
 public static class UserController {
   public static async Task<IResult> GetUser(int id, IUserService userService) {
-    try {
-      if (id <= 0) return Results.BadRequest(new { Error = "Invalid user id" });
-      var user = await userService.GetUserByIdAsync(id);
-      if (user is null) return Results.NotFound(new { Error = "User not found" });
-      else return Results.Ok(user);
-    } catch {
-      return Results.InternalServerError(new { Error = "Could not fetch user" });
-    }
+    if (id <= 0) return Results.BadRequest(new { Error = "Invalid user id" });
+    UserDto? user = await userService.GetUserByIdAsync(id);
+    if (user is null) return Results.NotFound(new { Error = "User not found" });
+    else return Results.Ok(user);
   }
 
   public static async Task<IResult> CreateUser(HttpRequest req, IUserService userService) {
+    UserModel? user;
     try {
-      var user = await req.ReadFromJsonAsync<UserModel>();
-      if (user is null) return Results.BadRequest(new { Error = "Invalid JSON" });
-      if (user.Validate() is string error) return Results.BadRequest(new { Error = error });
-      var id = await userService.CreateUserAsync(user);
-      return Results.Created($"/users/{id}", new { Id = id });
-    } catch {
-      return Results.InternalServerError(new { Error = "Could not create user" });
+      user = await req.ReadFromJsonAsync<UserModel>();
+    } catch (JsonException) {
+      return Results.BadRequest(new { Error = "Invalid JSON format" });
+    } catch (InvalidOperationException) {
+      return Results.BadRequest(new { Error = "Unsupported content type" });
     }
+    if (user is null) return Results.BadRequest(new { Error = "Invalid JSON data" });
+    user = user.Normalize();
+    if (user.Validate() is string error) return Results.BadRequest(new { Error = error });
+    int? id = await userService.CreateUserAsync(user);
+    if (id is null) return Results.InternalServerError(new { Error = "Could not create user" });
+    return Results.Created($"/users/{id}", new { Id = id });
   }
 }
