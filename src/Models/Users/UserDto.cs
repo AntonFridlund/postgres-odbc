e97b16e88c404555b0caedf79ca343ddf9bad4bb
Anton Fridlund <anton.fridlund@hotmail.se>
2025-10-02 22:34:26 +0200

    Move validation to model

--- src/Models/Users/UserDto.cs
+++ src/Models/Users/UserDto.cs
@@ -1,17 +1,8 @@
 namespace Models.Users;
 
-public partial record UserDto(
-    int Id,
-    string FirstName,
-    string LastName,
-    string Username
+public record UserDto(
+  int? Id,
+  string? FirstName,
+  string? LastName,
+  string? Username
 );
-
-public partial record UserDto {
-  public string? Validate() {
-    if (string.IsNullOrWhiteSpace(FirstName)) return "First name required";
-    if (string.IsNullOrWhiteSpace(LastName)) return "Last name required";
-    if (string.IsNullOrWhiteSpace(Username)) return "Username required";
-    return null;
-  }
-}
